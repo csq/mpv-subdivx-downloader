550cc871d91063994e82d78a046af1d5031f3863
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2025-02-17 11:16:02 -0300

    refactor: enhance function for checking directory subtitles

--- subdivx-downloader.lua
+++ subdivx-downloader.lua
@@ -20,18 +20,21 @@ function get_path_and_filename()
 end
 
 -- Function to check if subtitle exists in directory
-function exist_subtitle_in_dir(directory)
-    local files = io.popen('ls "'..directory..'"'):lines()
-    local extensions_subtitles = {'.srt', '.sub', '.ass', '.ssa', '.idx'}
+function does_subtitle_exist_in_directory(directory)
+    local file_names = io.popen('ls "'..directory..'"'):lines()
+    local subtitle_extensions = {'.srt', '.sub', '.ass', '.ssa', '.idx'}
 
-    for file in files do
-        for _, extension in ipairs(extensions_subtitles) do
-            if file:match(extension..'$') then
-                return true
+    for file_name in file_names do
+        for _, extension in ipairs(subtitle_extensions) do
+            if file_name:match(extension..'$') then
+                local expected_subtitle_name = mp.get_property('filename'):match('(.*)%..*') .. extension
+                if file_name == expected_subtitle_name then
+                    return true
+                end
             end
         end
     end
-    
+
     return false
 end
 
@@ -48,7 +51,7 @@ end
 function find_and_download_subtitle()
     local dir, fname = get_path_and_filename()
 
-    if exist_subtitle_in_dir(dir) then
+    if does_subtitle_exist_in_directory(dir) then
         local message = 'Subtitle already exists in directory'
 
         mp.msg.info(message)
